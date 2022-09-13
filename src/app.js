3a08a5c8d7c4d3a03801026664e355d0050d9639
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-09-13 15:27:25 -0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get;
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
