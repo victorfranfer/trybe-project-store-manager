d12beb7ff6c1036265827bdaffdd8f8a0a2b31df
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-09-13 19:38:42 -0300

    req4:feito

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const productController = require('../controllers/productController');
+const productMiddleware = require('../middlewares/productMiddleware');
 
 const product = express.Router();
 
 product.get('/', productController.getAll);
 product.get('/:id', productController.getProductById);
-product.post('/', productController.addProduct);
+product.post('/', productMiddleware.validateName, productController.addProduct);
 
 module.exports = product;
